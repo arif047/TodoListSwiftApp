f1d05f32ad438a80e37f8e0882fb80ad1f31a1c5
arif047 <94097287+arif047@users.noreply.github.com>
2023-08-01 10:41:21 +0600

    Update TodoListSwiftApp.swift

--- Shared/TodoListSwiftApp.swift
+++ Shared/TodoListSwiftApp.swift
@@ -4,7 +4,7 @@
 import SwiftUI
 
 @main
-struct TodoListAppTutorialApp: App {
+struct TodoListSwiftApp: App {
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
