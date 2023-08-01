b2738d077f214ad18b792eb381f22f7fa40516c9
arif047 <rarifkhan652@gmail.com>
2023-08-01 10:34:00 +0600

    TodoListSwiftApp
    TodoListSwiftApp

---
+++ Shared/TodoListSwiftApp.swift
@@ -0,0 +1,21 @@
+//  Created by Arifur Rahman
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppTutorialApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            
+            let context = persistenceController.container.viewContext
+            let dateHolder = DateHolder(context)
+            
+            TaskListView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(dateHolder)
+        }
+    }
+}
